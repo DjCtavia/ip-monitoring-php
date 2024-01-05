cbd5825c0a018dce70027f822c1b74559995a562
DjCtavia <djctavia@gmail.com>
2024-01-05 11:34:09 +0100

    feat(database): class and config

---
+++ database/DatabaseConfig.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace database;
+
+class DatabaseConfig
+{
+    public static string $host = "127.0.0.1";
+    public static string $username = "root";
+    public static string $password = "";
+    public static string $database = "ip_monitoring";
+}
---
+++ database/DatabaseConnection.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace database;
+
+use mysqli;
+
+require_once __DIR__ . '/DatabaseConfig.php';
+
+class DatabaseConnection
+{
+    private mysqli $conn;
+
+    public function __construct()
+    {
+        $this->connect();
+    }
+
+    private function connect(): void
+    {
+        $this->conn = new mysqli(DatabaseConfig::$host, DatabaseConfig::$username, DatabaseConfig::$password, DatabaseConfig::$database);
+
+        if ($this->conn->connect_error) {
+            die(["error" => "Connection to database failed: " . $this->conn->connect_error]);
+        }
+    }
+
+    public function getConnection(): mysqli
+    {
+        return $this->conn;
+    }
+
+    public function closeConnection(): void
+    {
+        $this->conn->close();
+    }
+}
